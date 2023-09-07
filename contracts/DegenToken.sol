9a0a0ea0db21d3f280966828d302f37acfac5d15
sabari245 <sabari.h.dev@gmail.com>
2023-09-07 19:29:26 +0530

    contract updated

---
+++ contracts/DegenToken.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Ownable {
+
+    constructor() ERC20("Degen", "DGN") {}
+
+    event Purchase(address indexed buyer, uint256 amount, string message);
+
+    modifier positiveAmount(uint256 amount) {
+        require(amount > 0, "Amount has to be greater than 0 wei");
+        _;
+    }
+
+    modifier hasBalance(uint256 amount) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public positiveAmount(amount) hasBalance(amount) {
+        _transfer(msg.sender, to, amount);
+    }
+
+    function redeem(uint256 amount, string message) public positiveAmount(amount) hasBalance(amount) {
+        _burn(msg.sender, amount);
+        emit Purchase(msg.sender, amount, message);
+    }
+
+    function balance() public view returns(uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function burn(uint256 amount) public positiveAmount(amount) hasBalance(amount) {
+        _burn(msg.sender, amount);
+    }
+}
